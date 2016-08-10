2d623c2a835036b3fb9d495228cc0d92272a7385
sriEakambaram <sridevire2013@gmail.com>
2016-08-10 16:56:29 +0530

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,15 @@
+public class Decimal {
+public static void main(String[] args) {
+String n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the binary value");
+n=s.next();
+int i=Integer.parseInt(n,2);
+System.out.println("decimal value:"+i);
+s.close();
+
+		
+
+	}
+
+}
