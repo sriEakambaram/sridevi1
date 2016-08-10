e0aaf64c9b0e40b01b831420a21251ea42dcc10b
sriEakambaram <sridevire2013@gmail.com>
2016-08-10 17:01:51 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+	public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=30;
+	System.out.println("values of a,b and c=:"+a+" "+b+" "+c+" ");
+	int temp=c;
+	c=b;
+	b=a;
+	a=temp;
+	System.out.println("values of a,b and c=:"+a+" "+b+" "+c+" ");
+}
+	
+
+	}
+
