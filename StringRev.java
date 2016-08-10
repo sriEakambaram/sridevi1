4a9351a93cac8008f31be9c28a8bf53fc0dd07be
sriEakambaram <sridevire2013@gmail.com>
2016-08-10 17:43:56 +0530

    Create StringRev.java

---
+++ StringRev.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StringRev
+{
+public stativ void main(String args[])
+{
+String s;
+Scanner s=new Scanner(System.in)
+StringBuffer sb=new StringBuffer(s);
+sb=sb.reverse();
+String s1=sb.tostring();
+System.out.println("reverse:"+s1);
+}
+}
